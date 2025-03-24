ba3b10ad5a27eb1f2a1758bf1537d5f34e0d2cb6
Goyal <KGOYAL2@jaguarlandrover.com>
2025-03-24 10:54:05 +0000

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+export default function Header() {
+    return (
+        <>
+            <div>
+                <div>
+                    <Image src={}/>
+                    <h1>NextZon</h1>
+
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </>
+    )
+}
